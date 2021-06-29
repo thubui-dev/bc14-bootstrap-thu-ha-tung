8039e940aa796c72597e45863ec69da64ed73d14
My Ha <vuonglanly1507@gmail.com>
2021-06-29 11:16:02 +0700

    add feature

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+owl.on('mousewheel', '.owl-stage', function (e) {
+    if (e.deltaY>0) {
+        owl.trigger('next.owl');
+    } else {
+        owl.trigger('prev.owl');
+    }
+    e.preventDefault();
+});
